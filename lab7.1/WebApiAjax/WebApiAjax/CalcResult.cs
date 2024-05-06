480ee2a7183851a56859dfff60e84b019fae522e
makaryorczyk <makaryorczyk@gmail.com>
2024-05-06 12:00:15 +0200

    Add ASP.NET Web API + Ajax assignment

---
+++ lab7.1/WebApiAjax/WebApiAjax/CalcResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WebApiAjax
+{
+	public class CalcResult
+	{
+        public int Sum { get; set; }
+        public int Difference { get; set; }
+        public int Product { get; set; }
+        public int Quotient { get; set; }
+    }
+}
+
